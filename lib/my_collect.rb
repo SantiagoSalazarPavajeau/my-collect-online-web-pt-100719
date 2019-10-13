e9c6e9d882158110c9918ea81336d9047022538f
Santiago Salazar <santiago.s.pavajeau@gmail.com>
2019-10-13 21:16:52 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
 
-
+def my_collect(array)
+  # my_collect is going to run on an array and each of its elements
+  i=0
+  # the counter is set to 0
+  collection = []
+  # we make an empty array to collect the new elements/iterations
+  while i < array.length
+# while the counter is less than the total amount of elements the loop still runs. So it runs as many times as needed to go through all the elements of the array.
+  
